b489748d0639897fd9b844e254d629bc11dc2445
lilsteviebird <stevenli880@gmail.com>
2022-04-27 20:23:57 -0400

    started part 5

--- sorting_starter/bsort.cc
+++ sorting_starter/bsort.cc
@@ -1,11 +1,15 @@
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
+#include <sys/timeb.h>
 
-#include "leveldb/include/leveldb/db.h"
+#include "leveldb/db.h"
 #include "json/json.h"
+#include <fstream>
+#include "library.h"
 
 using namespace std;
+
 
 int main(int argc, const char* argv[]) {
 
@@ -15,7 +19,68 @@ int main(int argc, const char* argv[]) {
 		exit(1);
 	}
 
-	// Do work here
+	//get inputs
+	string input_name = argv[1];
+	string output_name = argv[2];
+	vector <string> attributes;
+
+    for(int i = 3 ; i < argc; i++){
+        string curr = argv[i];
+        attributes.push_back(curr);
+    }
+
+
+	//create database
+	leveldb::DB *db;
+	leveldb::Options options;
+	options.create_if_missing = true;
+	leveldb::Status status = leveldb::DB::Open(options, "./leveldb_dir", &db);
+
+
+	//get schema here to find out the number of attributes;
+
+	///...
+
+	//we found out that we need to look at attributes... ex(first and third attributes in the list)
+
+	ifstream input_file(input_name);
+
+	long start = 0;
+    long finish = 0;
+    long time = 0;
+
+    struct timeb timer;
+    ftime(&timer);
+    start = timer.time*1000 + timer.millitm;
+
+	//write things to the tree
+	string line;
+	while(getline(input_file, line)){
+		if(!line.empty()){
+			if(status.ok()){
+				//need to add custom comparator for bottom line to work
+				// status = db->Put(leveldb::WriteOptions(), attributes, line);
+			}
+		}
+	}
+
+	ftime(&timer);
+    finish = timer.time*1000 + timer.millitm;
+    time = finish - start;
+    fprintf(stdout, "\nTIME: %ld milliseconds\n", time);
+
+	//now to iterate through each one and write it to file
+	ofstream output_file(output_name);
+
+	leveldb::Iterator* it = db->NewIterator(leveldb::ReadOptions());
+	for (it->SeekToFirst(); it->Valid(); it->Next()) {
+		cout << it->value().ToString() << endl;
+	}
+
+
+
 	
 	return 0;
 }
+
+
--- sorting_starter/library.h
+++ sorting_starter/library.h
@@ -1,4 +1,6 @@
 #include <cstdio>
+#include "leveldb/db.h"
+
 
 /**
  * An attribute schema. You should probably modify
