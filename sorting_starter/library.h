71b70182f4513ad266f64167f9837db05e8e0d41
Kamil Ahmed Aslam <kaslam2@u.rochester.edu>
2022-04-28 15:28:06 -0400

    updated files

--- sorting_starter/library.h
+++ sorting_starter/library.h
@@ -1,4 +1,7 @@
 #include <cstdio>
+#include <string>
+
+using namespace std;
 
 /**
  * An attribute schema. You should probably modify
@@ -35,37 +38,8 @@ typedef struct {
  * Creates sorted runs of length `run_length` in
  * the `out_fp`.
  */
-void mk_runs(FILE *in_fp, FILE *out_fp, long run_length, Schema *schema);
+void mk_runs(string in_fp, string out_fp, long run_length, Schema *schema);
 
-/**
- * The iterator helps you scan through a run.
- * you can add additional members as your wish
- */
-class RunIterator {
-  /**
-   * Creates an interator using the `buf_size` to
-   * scan through a run that starts at `start_pos`
-   * with length `run_length`.
-   */
-  RunIterator(FILE *fp, long start_pos, long run_length, long buf_size,
-              Schema *schema);
-
-  /**
-   * free memory
-   */
-  ~RunIterator();
-
-  /**
-   * reads the next record
-   */
-  Record* next();
-
-  /**
-   * return false if iterator reaches the end
-   * of the run
-   */
-  bool has_next();
-};
 
 /**
  * Merge runs given by the `iterators`.
@@ -73,6 +47,7 @@ class RunIterator {
  * Write the merged runs to `out_fp` starting at position `start_pos`.
  * Cannot use more than `buf_size` of heap memory allocated to `buf`.
  */
-void merge_runs(RunIterator* iterators[], int num_runs, FILE *out_fp,
+void merge_runs(int num_runs, FILE *out_fp,
                 long start_pos, char *buf, long buf_size);
+
 
